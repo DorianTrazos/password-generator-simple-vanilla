92f700221662e671b6b30b85fd03e076d69d7137
DorianCursos <doriancursos@gmail.com>
2024-06-24 12:16:01 +0200

    password generator simple

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,31 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+const passwordElement = document.getElementById('password');
+const lengthTextElement = document.getElementById('length-text');
+const rangeElement = document.getElementById('range');
+const buttonGenerateElement = document.getElementById('generate-password');
+
+const allowedCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890!@#$%^&*()_+-={}[]:;<>,.?/';
+
+let passwordLength = rangeElement.value;
+let finalPassword = '';
+
+const changeLengthText = event => {
+  passwordLength = event.target.value;
+  lengthTextElement.textContent = passwordLength;
+};
+
+const generatePassword = () => {
+  finalPassword = '';
+  for (let i = 0; i < passwordLength; i++) {
+    const randomPosition = Math.floor(Math.random() * allowedCharacters.length);
+    const randomCharacter = allowedCharacters.charAt(randomPosition);
+    finalPassword += randomCharacter;
+  }
+
+  passwordElement.value = finalPassword;
+};
+
+rangeElement.addEventListener('input', changeLengthText);
+buttonGenerateElement.addEventListener('click', generatePassword);
