b789f1e94411b34ef92385fb94c45a6deeebc7cf
Jose Diaz <doriancursos@gmail.com>
2024-06-27 16:44:06 +0200

    password simple

--- src/js/index.js
+++ src/js/index.js
@@ -6,71 +6,33 @@ const lengthTextElement = document.getElementById('length-text');
 const rangeElement = document.getElementById('range');
 const buttonGenerateElement = document.getElementById('generate-password');
 
-const uppercaseInputElement = document.getElementById('uppercase');
-const lowercaseInputElement = document.getElementById('lowercase');
-const numbersInputElement = document.getElementById('numbers');
-const symbolsInputElement = document.getElementById('symbols');
-
-const passwordOptions = {
-  uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-  lowercase: 'abcdefghijklmnopqrstuvwxyz',
-  numbers: '0123456789',
-  symbols: '!@#$%^&*()_+-={}[]:;<>,.?/'
-};
+const allowedCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890!@#$%^&*()_+-={}[]:;<>,.?/';
 
 let passwordLength = rangeElement.value;
-let allowedCharacters = '';
-let finalPassword = '';
 
-const setDisabledButton = () => {
-  buttonGenerateElement.disabled = !allowedCharacters.length;
+const generateRandomNumber = () => {
+  return Math.floor(Math.random() * allowedCharacters.length);
 };
 
-const fillAllowedCharacters = () => {
-  allowedCharacters = '';
-  const checkboxes = document.querySelectorAll('input:checked');
-
-  if (checkboxes.length === 0) {
-    return;
+const generatePassword = () => {
+  let newPassword = '';
+  for (let i = 0; i < passwordLength; i++) {
+    const randomNumber = generateRandomNumber();
+    // newPassword += allowedCharacters.charAt(randomNumber);
+    newPassword += allowedCharacters[randomNumber];
   }
-
-  checkboxes.forEach(input => (allowedCharacters += passwordOptions[input.id]));
-
-  console.log(allowedCharacters);
-
-  setDisabledButton();
+  return newPassword;
 };
 
-const changeLengthText = event => {
+const printPassword = () => {
+  passwordElement.value = generatePassword();
+};
+
+const setPasswordLength = event => {
   passwordLength = event.target.value;
   lengthTextElement.textContent = passwordLength;
 };
 
-const generateRandomPosition = () => {
-  const randomPosition = Math.floor(Math.random() * allowedCharacters.length);
-  return randomPosition;
-};
+rangeElement.addEventListener('input', setPasswordLength);
 
-const getRandomCharacter = () => {
-  const randomPosition = generateRandomPosition();
-  const randomCharacter = allowedCharacters.charAt(randomPosition);
-  return randomCharacter;
-};
-
-const generatePassword = () => {
-  finalPassword = '';
-  for (let i = 0; i < passwordLength; i++) {
-    const randomCharacter = getRandomCharacter();
-    finalPassword += randomCharacter;
-  }
-
-  passwordElement.value = finalPassword;
-};
-
-rangeElement.addEventListener('input', changeLengthText);
-buttonGenerateElement.addEventListener('click', generatePassword);
-
-uppercaseInputElement.addEventListener('change', fillAllowedCharacters);
-lowercaseInputElement.addEventListener('change', fillAllowedCharacters);
-numbersInputElement.addEventListener('change', fillAllowedCharacters);
-symbolsInputElement.addEventListener('change', fillAllowedCharacters);
+buttonGenerateElement.addEventListener('click', printPassword);
